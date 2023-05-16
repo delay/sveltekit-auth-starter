ab5db9b63d6200a4846777a5d5f221fcb1db2336
Jeff McMorris <delay@pobox.com>
2023-05-16 09:40:03 -0500

    minor name updates

--- src/lib/config/constants.ts
+++ src/lib/config/constants.ts
@@ -5,11 +5,11 @@ export const CONTACT_EMAIL = 'support@superbuild.pro';
 /* WARNING!!! TERMS AND CONDITIONS AND PRIVACY POLICY 
 WERE CREATED BY CHATGPT AS AN EXAMPLE ONLY. 
 CONSULT A LAWYER AND DEVELOP YOUR OWN TERMS AND PRIVACY POLICY!!! */
-export const TERMS_PRIVACY_CONTACT_EMAIL = 'support@superbuild.pro';
-export const TERMS_PRIVACY_WEBSITE = 'superbuild.pro';
-export const TERMS_PRIVACY_COMPANY = 'SuperBuild.Pro';
+export const TERMS_PRIVACY_CONTACT_EMAIL = 'yourname@email.com';
+export const TERMS_PRIVACY_WEBSITE = 'yourdomain.com';
+export const TERMS_PRIVACY_COMPANY = 'Your Company';
 export const TERMS_PRIVACY_EFFECTIVE_DATE = 'January 1, 2023';
-export const TERMS_PRIVACY_APP_NAME = 'SuperBuild.Pro';
+export const TERMS_PRIVACY_APP_NAME = 'Your App';
 export const TERMS_PRIVACY_APP_PRICING_AND_SUBSCRIPTIONS =
 	'[Details about the pricing, subscription model, refund policy]';
 export const TERMS_PRIVACY_COUNTRY = 'United States';
