a5275550845c4d49f189c573ded5b8a428f2ceb6
Robin Maasjosthusmann <r.maasjosthusmann@partnery.de>
2023-05-18 14:45:19 +0200

    fix(auth): Move auth for protected routes into hook

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,7 +1,14 @@
 import { auth } from '$lib/server/lucia';
-import type { Handle } from '@sveltejs/kit';
+import { redirect, type Handle } from '@sveltejs/kit';
 
 export const handle: Handle = async ({ event, resolve }) => {
 	event.locals.auth = auth.handleRequest(event);
+
+	if (event.url.pathname.startsWith('/protected')) {
+		const { user } = await event.locals.auth.validateUser();
+		if (!user) throw redirect(302, '/auth/sign-in');
+		if (!user.verified) throw redirect(302, '/auth/verify/email');
+	}
+
 	return await resolve(event);
 };
